9ff273eb71d9f4d9ab7c752d6abe05720a253c2d
faithnicoletti <faithalicia22@gmail.com>
2023-08-01 17:12:05 -0400

    navbar functionality

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <nav>
+            <Link to="/">All Jackets</Link>
+            &nbsp; | &nbsp;
+            <Link to="/custom">Customize Your Own?</Link>
+            &nbsp; | &nbsp;
+            <Link to="/checkout">Checkout</Link>
+            &nbsp; | &nbsp;
+            <Link to="/login">Login</Link>
+            &nbsp; | &nbsp;
+            <Link to="/sign-up">Sign-up</Link>
+        </nav>
+    );
+}
---
+++ client/src/components/login/login.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Custom() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email, password);
+    axios.post("http://localhost:3001/login-user", {
+      email,
+      password,
+    })
+      .then((response) => {
+        const data = response.data;
+        console.log(data, "userRegister");
+        if (data.status === "ok") {
+          alert("login successful");
+          window.localStorage.setItem("token", data.data);
+          window.localStorage.setItem("loggedIn", true);
+          window.location.href = "./myusers";
+        }
+      })
+      .catch((error) => {
+        console.error("Error logging in:", error);
+      });
+  }
+  return (
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+        <form onSubmit={handleSubmit}>
+          <h3>Sign In</h3>
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <div className="custom-control custom-checkbox">
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+              />
+              <label className="custom-control-label" htmlFor="customCheck1">
+                Remember me
+              </label>
+            </div>
+          </div>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            <a href="/sign-up">Sign Up</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
